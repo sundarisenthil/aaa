659b32ec54d75e0305b4cebf51d8dc0ed0becdcb
sundarisenthil <sundariss11@gmail.com>
2016-08-13 12:13:52 +0530

    Add files via upload

---
+++ Rev.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+
+class Rev
+{
+
+public static void main(String ar[]){
+
+
+Scanner sc=new Scanner(System.in);
+
+StringBuffer sb=new StringBuffer(sc.next());
+ 
+sb.reverse();
+
+System.out.println(sb);
+
+}}
