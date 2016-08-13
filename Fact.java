aa29326429085288002543b4a2f960bf1347f411
sundarisenthil <sundariss11@gmail.com>
2016-08-13 12:21:30 +0530

    Add files via upload

---
+++ Fact.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+
+class Fact
+{
+
+public static void main(String ar[]){
+
+
+Scanner sc=new Scanner(System.in);
+
+int n=sc.nextInt();
+
+int fact=1;
+
+for(int i=1;i<=n;i++)
+{
+
+fact=fact*i;
+
+
+}
+System.out.println(fact);
+
+}}
